965f1cd9b0dcbedebcd66d81b9fffe64d84258e6
charles-freitas <ccmfreitas@gmail.com>
2025-11-04 20:10:22 -0300

    Upload do exemplo de árvores BST

--- 2025-11-04 - Arvores BST/ArvoreBinariaBusca.java
+++ 2025-11-04 - Arvores BST/ArvoreBinariaBusca.java
@@ -8,41 +8,49 @@ public class ArvoreBinariaBusca<T extends Comparable<T>> implements ArvoreBinari
       this.raiz = null;
    }
 
+   // Implementação do método que insere um nó
    @Override
    public void inserirNo(T dado) {
 
    } 
 
+   // Implementação do método que busca um nó
    @Override
    public NoBinario<T> buscarNo(T dado) {
 
    }
 
+   // Implementação do método que exclui um nó
    @Override
    public void excluirNo(T dado) {
 
    }
 
+   // Implementação do método que imprime a árvore em pré-ordem
    @Override
    public void imprimirPreOrdem() {
 
    }
 
+   // Implementação do método que imprime a árvore em pós-ordem
    @Override
    public void imprimirPosOrdem() {
 
    }
 
+   // Implementação do método que imprime a árvore em ordem
    @Override
    public void imprimirNaOrdem() {
 
    }
 
+   // Implementação do método que retorna a raiz da árvore
    @Override
    public NoBinario<T> getRaiz() { 
 
    }
 
+   // Implementação do método que verifica se a árvore está vazia
    @Override
    public boolean isVazia() { 
 
